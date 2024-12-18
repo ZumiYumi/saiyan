b74fa0db43ec5d17a3a754326a0632dd4408003e
Zumi Yumi <150757931+ZumiYumi@users.noreply.github.com>
2024-12-17 16:21:22 -0800

    Update blooudhound.sql

--- scripts/enumeration/ad/blooudhound.sql
+++ scripts/enumeration/ad/blooudhound.sql
@@ -13,3 +13,13 @@ RETURN g
 MATCH (owningGroup:Group)-[:Owns]->(ownedGroup:Group)
 WHERE ownedGroup.name = 'MANAGEMENT DEPARTMENT@CORP.COM'
 RETURN owningGroup
+--find all computer
+MATCH (m:Computer) RETURN m
+--find all users
+MATCH (m:User) RETURN m
+--final all active sessions
+MATCH p = (c:Computer)-[:HasSession]->(m:User) RETURN p
+--search for WMI vulnerable
+MATCH (m:User)
+WHERE compromised = 'True'
+
